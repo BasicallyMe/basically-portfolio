ac3a15240d2b4e736a3aa467494912758bd06d9b
Abajit Dey <abajitdeyag10118@gmail.com>
2022-02-21 21:30:27 +0530

    added the random words section

--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
 import './styles.scss'
+import randomWordsGenerate from 'random-words'
 
-console.log("Hello World")
+const randomQuote = document.getElementById('random-quote')
+randomQuote.innerHTML = `basically-${randomWordsGenerate({  exactly: 1, minLength: 5 }).toString()}`;  
