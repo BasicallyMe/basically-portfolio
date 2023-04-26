aa15b8e3473227c9f543ef336e21abb416e374f0
Abajit Dey <abajitdeyag10118@gmail.com>
2023-04-26 09:30:11 +0530

    Added footer section
    Updated footer section, added social icons and text for footer card. Also added links to mail and resume

--- .eleventy.js
+++ .eleventy.js
@@ -18,14 +18,34 @@ async function imageShortcode(src, alt, sizes) {
   return Image.generateHTML(metadata, imageAttributes);
 }
 
+async function svgShortcode(src, alt, sizes) {
+  let metadata = await Image(`./src${src}`, {
+    widths: [30, 32, null],
+    formats: ["svg"],
+    urlPath: "/icons/",
+    outputDir: "./public/icons/",
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css/");
   eleventyConfig.addWatchTarget("./src/css/");
   eleventyConfig.addPassthroughCopy("./src/images/");
+  eleventyConfig.addPassthroughCopy("./src/icons/");
   eleventyConfig.addPassthroughCopy("./src/pdfs/");
   eleventyConfig.addPassthroughCopy({ "./src/favicons": "/" });
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
   eleventyConfig.addNunjucksAsyncShortcode("EleventyImage", imageShortcode);
+  eleventyConfig.addNunjucksAsyncShortcode("EleventySVG", svgShortcode);
 
   return {
     dir: {
